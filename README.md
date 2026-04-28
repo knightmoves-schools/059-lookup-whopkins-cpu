97ebd2fd2552dc00c6c4770e231f96f752f1262c
Dawn DeWaay <99098265+DawnDeWaay@users.noreply.github.com>
2026-04-28 12:00:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# 059 Lookup
+
+[![059 Lookup](https://img.youtube.com/vi/JjXSbBRyLZs/0.jpg)](https://www.youtube.com/watch?v=JjXSbBRyLZs)
+
+the index.js file
+- should create a `directions` lookup object that looks up the four abriviations `N`, `S`, `E` and `W` and returns their corresponding full name like `North`
+
+resources:
+- https://www.w3schools.com/js/js_objects.asp
+
+Copyright &copy; 2023 Knight Moves. All Rights Reserved.
---
+++ test/verify.test.js
@@ -0,0 +1,52 @@
+const http = require("http");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const { assert } = require("console");
+
+let server;
+let browser;
+let page;
+
+beforeAll(async () => {
+  server = http.createServer(function (req, res) {
+    fs.readFile(__dirname + "/.." + req.url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  });
+
+  server.listen(process.env.PORT || 3000);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/index.html");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+describe('the index.js file', () => {
+  it('should create a `directions` lookup object that looks up the four abriviations `N`, `S`, `E` and `W` and returns their corresponding full name like `North`', async function() {
+      const directions = await page.evaluate(() => directions);
+      expect(directions.N).toBe('North');
+      expect(directions.E).toBe('East');
+      expect(directions.S).toBe('South');
+      expect(directions.W).toBe('West');
+  });
+});
+
